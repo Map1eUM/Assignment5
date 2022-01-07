492ebb68f6c6dc594a9c8504fdb748a7523c90f5
Map1eUM <chenr5@myumanitoba.ca>
2022-01-06 22:57:15 -0600

    First Question basically done, minor visual bug exist

--- src/Assignment5.java
+++ src/Assignment5.java
@@ -1,16 +1,72 @@
 import processing.core.PApplet;
 
-public class Assignment5 extends PApplet{
-    public void settings(){
-        size(200, 200);
+public class Assignment5 extends PApplet {
+    /*
+ COMP 1010 Fall 2021
+ Assignment 5 - Units 1-19 - Minesweeper
+ Question 1 Template
+ */
+
+//Q1: Set up a grid on the canvas. Draw a "mine" cell
+//    and a "number of mines nearby" cell.
+
+    final int CELLSIZE = 50;
+    final int NUM_ROWS = 10;
+    final int NUM_COLS = 12;
+    final int ARROW_WIDTH = CELLSIZE / 10;
+    final int ARROW_HEIGHT = CELLSIZE / 2;
+    final int MINE_FIGURE_CIRCLE_DIAMETER = ARROW_WIDTH;
+    final int FIGURE_MARGIN = 10;
+    final int TEXT_MARGIN = FIGURE_MARGIN;
+    final int TEXT_STROKE = 8;
+    final int TEXT_SIZE = 40;
+    //colors
+    final int WHITE = color(255);
+    final int RED = color(255, 0, 0);
+    final int BLACK = color(0);
+    final int BLUE = color(0, 0, 255);
+
+    public void settings() {
+        size(NUM_COLS * CELLSIZE, NUM_ROWS * CELLSIZE);
     }
 
-    public void draw(){
-        background(0);
-        ellipse(mouseX, mouseY, 20, 20);
+    public void setup() {
+//        size(600, 500); //size MUST be (NUM_COLS*CELLSIZE) by (NUM_ROWS*CELLSIZE);
+        noLoop(); //only draw once
     }
 
-    public static void main(String... args){
+    public void draw() {
+        drawMine(3, 4);  //draw a mine in col 3, row 4
+        drawMine(1, 0);  //draw a mine in col 1, row 0
+        drawMine(8, 7);  //draw a mine in col 8, row 7
+        drawMine(11, 9);  //draw a mine in col 11, row 9
+
+        drawNum(0, 2, 3); //draw a 3 in col 0, row 2
+        drawNum(10, 3, 1); //draw a 1 in col 10, row 3
+        drawNum(5, 6, 4); //draw a 4 in col 5, row 6
+        drawNum(2, 8, 2); //draw a 2 in col 2, row 8
+    }
+
+    //**add your drawMine function here**
+    void drawMine(int column, int row) {
+        fill(WHITE);
+        rect(CELLSIZE * column, CELLSIZE * row, CELLSIZE, CELLSIZE);
+        fill(RED);
+        ellipse(CELLSIZE * column + CELLSIZE / 2, CELLSIZE * row + CELLSIZE / 2, CELLSIZE, CELLSIZE);
+        fill(BLACK);
+        rect(column * CELLSIZE + CELLSIZE / 2 - ARROW_WIDTH / 2, row * CELLSIZE + CELLSIZE / 2 - 2 * ARROW_HEIGHT / 3, ARROW_WIDTH, ARROW_HEIGHT);
+        ellipse(CELLSIZE * column + CELLSIZE / 2 , row * CELLSIZE + CELLSIZE / 2 + ARROW_HEIGHT / 3 + FIGURE_MARGIN, MINE_FIGURE_CIRCLE_DIAMETER, MINE_FIGURE_CIRCLE_DIAMETER);
+    }
+
+    //**add your drawNum function here**
+    void drawNum(int column, int row, int num) {
+        fill(BLUE);
+        stroke(TEXT_STROKE);
+        textSize(TEXT_SIZE);
+
+    }
+
+    public static void main(String... args) {
         PApplet.main("Assignment5");
     }
 }
