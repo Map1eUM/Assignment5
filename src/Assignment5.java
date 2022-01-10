e66584d6dfc95255d9896b8d28e2ae1ad55a5c32
Map1eUM <chenr5@myumanitoba.ca>
2022-01-10 00:10:14 -0600

    Q2 basically done.
    remember in the end change all the code back to processing way and template code must be restored to the same as it was downloaded

--- src/Assignment5.java
+++ src/Assignment5.java
@@ -1,14 +1,16 @@
 import processing.core.PApplet;
 
 public class Assignment5 extends PApplet {
-    /*
+ /*
  COMP 1010 Fall 2021
  Assignment 5 - Units 1-19 - Minesweeper
- Question 1 Template
+ Q2 Template
  */
 
 //Q1: Set up a grid on the canvas. Draw a "mine" cell
 //    and a "number of mines nearby" cell.
+//Q2: Add functions to draw the grid, a list of mines and
+//    a list of number cells.
 
     final int CELLSIZE = 50;
     final int NUM_ROWS = 10;
@@ -17,7 +19,7 @@ public class Assignment5 extends PApplet {
     final int ARROW_HEIGHT = CELLSIZE / 2;
     final int TRIG_HEIGHT = ARROW_WIDTH;
     final int FIGURE_MARGIN = 5;
-//    final int TEXT_MARGIN = FIGURE_MARGIN;
+    //    final int TEXT_MARGIN = FIGURE_MARGIN;
     final int TEXT_STROKE = 8;
     final int TEXT_SIZE = 40;
     //colors
@@ -26,28 +28,73 @@ public class Assignment5 extends PApplet {
     final int BLACK = color(0);
     final int BLUE = color(0, 0, 255);
     final int YELLOW = color(255, 255, 0);
-
+    //the worst situation is all grid cells are mines.
+    int[] mineX = new int[NUM_COLS * NUM_ROWS];
+    int[] mineY = new int[NUM_COLS * NUM_ROWS];
     public void settings() {
         size(NUM_COLS * CELLSIZE, NUM_ROWS * CELLSIZE);
     }
-
     public void setup() {
-//        size(600, 500); //siz`e MUST be (NUM_COLS*CELLSIZE) by (NUM_ROWS*CELLSIZE);
+//        size(600, 500); //size MUST be (NUM_COLS*CELLSIZE) by (NUM_ROWS*CELLSIZE);
         noLoop(); //only draw once
     }
 
     public void draw() {
-        drawMine(3, 4);  //draw a mine in col 3, row 4
-        drawMine(1, 0);  //draw a mine in col 1, row 0
-        drawMine(8, 7);  //draw a mine in col 8, row 7
-        drawMine(11, 9);  //draw a mine in col 11, row 9
+        drawGrid();
 
-        drawNum(0, 2, 3); //draw a 3 in col 0, row 2
-        drawNum(10, 3, 1); //draw a 1 in col 10, row 3
-        drawNum(5, 6, 4); //draw a 4 in col 5, row 6
-        drawNum(2, 8, 2); //draw a 2 in col 2, row 8
+        //create test arrays
+        int[] mineX = {3, 1, 8, 11, 8, 4}; //mine column numbers
+        int[] mineY = {4, 0, 7, 9, 2, 1}; //mine row numbers
+
+        drawMines(mineX, mineY, 6); //fill the cells with mines
+
+        int[] numX = {0, 10, 5, 2, 6, 7, 1, 11, 6};
+        int[] numY = {2, 3, 6, 8, 0, 9, 6, 1, 4};
+        int[] numValue = {3, 1, 4, 2, 5, 6, 0, 7, 8};
+
+        drawNums(numX, numY, numValue, 9);
     }
 
+    //**add your drawGrid function here**
+    void drawGrid() {
+        for (int i = 0; i < NUM_COLS; ++i) {
+            for (int j = 0; j < NUM_ROWS; ++j) {
+                rect(CELLSIZE * i, CELLSIZE * j, CELLSIZE, CELLSIZE);
+            }
+        }
+
+    }
+
+    //**add your drawMines function here**
+    void drawMines(int[] column, int[] row, int numMine) {
+        for (int i = 0; i < numMine; ++i) {
+            fill(WHITE);
+            rect(CELLSIZE * column[i], CELLSIZE * row[i], CELLSIZE, CELLSIZE);
+            fill(YELLOW);
+            ellipse(CELLSIZE * column[i] + CELLSIZE / 2, CELLSIZE * row[i] + CELLSIZE / 2, CELLSIZE, CELLSIZE);
+            fill(BLACK);
+            rect(column[i] * CELLSIZE + CELLSIZE / 2 - ARROW_WIDTH / 2, row[i] * CELLSIZE + CELLSIZE / 2 - 2 * ARROW_HEIGHT / 3, ARROW_WIDTH, ARROW_HEIGHT);
+            float x1 = column[i] * CELLSIZE + CELLSIZE / 2;
+            float y1 = row[i] * CELLSIZE + CELLSIZE / 2 + ARROW_HEIGHT / 3 + FIGURE_MARGIN;
+            triangle(x1, y1, x1 - ARROW_WIDTH / 2, y1 + TRIG_HEIGHT, x1 + ARROW_WIDTH / 2, y1 + TRIG_HEIGHT);
+            noFill();
+        }
+    }
+
+    //**add your drawNums function here**
+    void drawNums(int[] column, int[] row, int[] val, int numNumber) {
+        for (int i = 0; i < numNumber; ++i) {
+            fill(WHITE);
+            strokeWeight(1);
+            rect(CELLSIZE * column[i], CELLSIZE * row[i], CELLSIZE, CELLSIZE);
+            fill(BLUE);
+            strokeWeight(TEXT_STROKE);
+            textSize(TEXT_SIZE);
+            text(str(val[i]), CELLSIZE * column[i] + CELLSIZE / 2 - textWidth(str(val[i])) / 2, CELLSIZE * (row[i] + 1) - textDescent());
+        }
+    }
+
+    //**paste your drawMine and drawNum functions from Q1 here**
     //**add your drawMine function here**
     void drawMine(int column, int row) {
         fill(WHITE);
@@ -64,19 +111,15 @@ public class Assignment5 extends PApplet {
 
     //**add your drawNum function here**
     void drawNum(int column, int row, int num) {
-//        fill(BLUE);
         fill(WHITE);
         strokeWeight(1);
         rect(CELLSIZE * column, CELLSIZE * row, CELLSIZE, CELLSIZE);
-//        stroke(BLUE);
+
         fill(BLUE);
         strokeWeight(TEXT_STROKE);
         textSize(TEXT_SIZE);
         text(str(num), CELLSIZE * column + CELLSIZE / 2 - textWidth(str(num)) / 2, CELLSIZE * (row + 1) - textDescent());
-//        strokeWeight(1);
-//        stroke(BLACK);
     }
-
     public static void main(String... args) {
         PApplet.main("Assignment5");
     }
